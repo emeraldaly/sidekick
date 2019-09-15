80de0336a83089785e9b73aff71bccf60301d592
Alyson <emerald.aly@gmail.com>
2019-09-15 10:08:37 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var PORT = process.env.PORT || 8080;
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+  console.log("listening on PORT:" + PORT);
+});
